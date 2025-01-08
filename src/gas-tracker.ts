5aa511d490fd7194d8fb2902f0eb1911b06fdd69
MartinGbz <martin.grabarz@outlook.fr>
2025-01-08 16:56:09 +0100

    ♻️ refactor code

--- src/gas-tracker.ts
+++ src/gas-tracker.ts
@@ -18,6 +18,12 @@ export type DelegatePlatformFees = {
 const trackAddressesGas = async () => {
   requireEnv();
 
+  if (!process.env.START_BLOCK || !process.env.END_BLOCK) {
+    throw new Error("Missing start or end block");
+  }
+  const startBlock = BigInt(process.env.START_BLOCK);
+  const endBlock = BigInt(process.env.END_BLOCK);
+
   const delegatePlatformsWithFees: DelegatePlatformFees[] = [];
 
   for (const delegatePlatform of delegatePlatforms) {
@@ -27,7 +33,7 @@ const trackAddressesGas = async () => {
     });
 
     for (const address of delegatePlatform.addresses) {
-      const gas = await getAllGasUsed(address);
+      const gas = await getAllGasUsed(address, startBlock, endBlock);
       delegatePlatformsWithFees[
         delegatePlatformsWithFees.length - 1
       ].addresses.push({
@@ -45,14 +51,19 @@ const trackAddressesGas = async () => {
  * @param address
  * @returns
  */
-const getAllGasUsed = async (address: Address) => {
+const getAllGasUsed = async (
+  address: Address,
+  startBlock: bigint,
+  endBlock: bigint
+) => {
   const history = await etherscanProvider.getHistory(
     address,
-    process.env.START_BLOCK,
-    process.env.END_BLOCK
+    startBlock,
+    endBlock
   );
+  console.log(`🔍 Found ${history.length} transactions for ${address}`);
   const totalGasUsed = history.reduce(
-    (acc, curr) => acc + BigInt(curr.gasUsed) * BigInt(curr.gasPrice),
+    (acc, cur) => acc + BigInt(cur.gasUsed) * BigInt(cur.gasPrice),
     0n
   );
   return totalGasUsed;
